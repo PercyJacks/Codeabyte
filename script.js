89c6bf39f2ebf3e8a7c4bb4d02303ce52c79e21f
Percy <percyjacks@github.com>
2024-01-09 17:48:44 +0000

    Get date of most recent commit in each repo

--- script.js
+++ script.js
@@ -17,11 +17,26 @@ const getCommit = async (repo) => {
   return data;
 };
 
+// Get current date
+currentDate = new Date();
+// Avoid comparing time for current date
+currentDate.setHours(0,0,0,0);
+
 getRepos().then((repos) => {
   for (const repo of repos) {
     getCommit(repo.name).then((commits) => {
+      let latestCommit = commits[0];
       // Check that name matches name of user
+      // For some commits, author is null?? If author is null, assume it was done by user?
       // Check that date matches today's date
-    })
+      commitDate = new Date(latestCommit.commit.author.date);
+      if (currentDate < commitDate) {
+        console.log(`Repo: ${repo.name}. Commit Date: ${commitDate.toLocaleDateString()}. This commit is in date. NICE!!!`);
+        // Tally points
+      } else {
+        console.log(`Repo: ${repo.name}. Commit Date: ${commitDate.toLocaleDateString()}. This is an old commit!`);
+        // Send email reminder
+      }
+    });
   }
-})
+});
