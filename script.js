db1bacae3d90364ff37ee8be52bfb583876f1bff
Percy <percyjacks@github.com>
2024-01-10 16:46:15 +0000

    Checking for author of commit

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 
 username = 'PercyJacks'
-
+firstname = 'Percy'
 // Function to get all the repos for a particular user
 const getRepos = async () => {
   const endpoint = new URL(`https://api.github.com/users/${username}/repos`);
@@ -27,15 +27,18 @@ getRepos().then((repos) => {
     getCommit(repo.name).then((commits) => {
       let latestCommit = commits[0];
       // Check that name matches name of user
-      // For some commits, author is null?? If author is null, assume it was done by user?
-      // Check that date matches today's date
-      commitDate = new Date(latestCommit.commit.author.date);
-      if (currentDate < commitDate) {
-        console.log(`Repo: ${repo.name}. Commit Date: ${commitDate.toLocaleDateString()}. This commit is in date. NICE!!!`);
-        // Tally points
-      } else {
-        console.log(`Repo: ${repo.name}. Commit Date: ${commitDate.toLocaleDateString()}. This is an old commit!`);
-        // Send email reminder
+      // For some commits, the name is different but usually contains the first name so check for that
+      let commitAuthorName = latestCommit.commit.author.name
+      if (commitAuthorName.toLowerCase().includes(firstname.toLowerCase())) {
+        // Check that date matches today's date
+        commitDate = new Date(latestCommit.commit.author.date);
+        if (currentDate < commitDate) {
+          console.log(`Repo: ${repo.name}. Commit Date: ${commitDate.toLocaleDateString()}. This commit is in date. NICE!!!`);
+          // Tally points
+        } else {
+          console.log(`Repo: ${repo.name}. Commit Date: ${commitDate.toLocaleDateString()}. This is an old commit!`);
+          // Send email reminder
+        }
       }
     });
   }
