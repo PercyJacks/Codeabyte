c35336f4b569f136b81c512ea672fa04395490a1
Percy <percyjacks@github.com>
2024-01-12 19:21:48 +0000

    Separating functionality into async functions

--- script.js
+++ script.js
@@ -14,55 +14,41 @@ const getCommit = async (repo) => {
   const endpoint = new URL(`https://api.github.com/repos/${username}/${repo}/commits`);
   const response = await fetch(endpoint);
   const data = await response.json();
-  return data;
+  return data[0];
 };
 
-// Get current date
-currentDate = new Date();
-// Avoid comparing time for current date
-currentDate.setHours(0,0,0,0);
-commitsMade = 0;
-
-//
-//
-//
-
-// let repoArray = [];
-
-// async function getRepos2() {
-//   const endpoint = new URL(`https://api.github.com/users/${username}/repos`);
-//   const response = await fetch(endpoint);
-//   const data = await response.json();
-//   return data;
-// }
-
-// await Promise.all(repoArray.push(getRepos2))
-// console.log(repoArray);
-
-// Get the number of commits made today
-getRepos().then((repos) => {
+// Async function to tally Commits
+const tallyCommits = async () => {
+  let commitsMade = 0;
+  // Get current date
+  let currentDate = new Date();
+  // Avoid comparing time for current date
+  currentDate.setHours(0,0,0,0);
+  const repos = await getRepos();
   for (const repo of repos) {
-    getCommit(repo.name).then(async (commits) => {
-      let latestCommit = commits[0];
-      // Check that name matches name of user
-      // For some commits, the name is different but usually contains the first name so check for that
-      let commitAuthorName = latestCommit.commit.author.name
-      if (commitAuthorName.toLowerCase().includes(firstname.toLowerCase())) {
-        // Check that date matches today's date
-        commitDate = new Date(latestCommit.commit.author.date);
-        if (currentDate < commitDate) {
-          console.log(`Repo: ${repo.name}. Commit Date: ${commitDate.toLocaleDateString()}. This commit is in date. NICE!!!`);
-          // Tally points
-          await Promise.all(commitsMade += 1);
-        } else {
-          // console.log(`Repo: ${repo.name}. Commit Date: ${commitDate.toLocaleDateString()}. This is an old commit!`);
-          // Send email/notification reminder
-          // alert("Make a commit before the day is over!");
-        }
-      }
-    });
-  }
-  console.log("Inside", commitsMade);
-});
+    let latestCommit = await getCommit(repo.name);
+    let commitAuthorName = latestCommit.commit.author.name;
+    if (commitAuthorName.toLowerCase().includes(firstname.toLowerCase())) {
+      // Check that date matches today's date
+      commitDate = new Date(latestCommit.commit.author.date);
+      if (currentDate < commitDate) {
+        console.log(`Repo: ${repo.name}. Commit Date: ${commitDate.toLocaleDateString()}. This commit is in date. NICE!!!`);
+        // Tally points
+        commitsMade += 1;
+      } else {
+        // Handled elsewhere
+        // console.log(`Repo: ${repo.name}. Commit Date: ${commitDate.toLocaleDateString()}. This is an old commit!`);
 
-console.log("Outside", commitsMade);
+      }
+    }
+  }
+  return commitsMade;
+};
+
+tallyCommits()
+
+// Function to handle notification/email
+// Send email/notification reminder
+        // alert("Make a commit before the day is over!");
+
+// Function to handle rewards
