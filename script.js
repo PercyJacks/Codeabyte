23eb21e8018b58d5ca791919ca36079c82f67744
Percy <percyjacks@github.com>
2024-01-14 20:15:48 +0000

    Organising code

--- script.js
+++ script.js
@@ -45,6 +45,21 @@ const tallyCommits = async () => {
   return commitsMade;
 };
 
+// How about I create a function that takes in a message to send as a notification? That
+// way it can handle both types of notifications ("good job" | "Make a commit before the day is over")
+// Maybe take in extra kawrgs to add to the notification e.g. option to take in a link
+const notify = (message) => {
+  // Figure out how to display the message like a notification in an app
+  Notification.requestPermission().then(perm => {
+    if (perm === "granted") {
+      new Notification("Codeabyte", {
+        body: message,
+        icon: "", // Icon for codeabyte maybe?
+      });
+    }
+  });
+};
+
 // Function to handle rewards
 const handleRewards = async () => {
   // If commitsMade is 1 or more, send a notification
@@ -61,19 +76,3 @@ const handleRewards = async () => {
 };
 
 handleRewards();
-
-
-// How about I create a function that takes in a message to send as a notification? That
-// way it can handle both types of notifications ("good job" | "Make a commit before the day is over")
-// Maybe take in extra kawrgs to add to the notification e.g. option to take in a link
-const notify = (message) => {
-  // Figure out how to display the message like a notification in an app
-  Notification.requestPermission().then(perm => {
-    if (perm === "granted") {
-      new Notification("Codeabyte", {
-        body: message,
-        icon: "", // Icon for codeabyte maybe?
-      });
-    }
-  });
-};
